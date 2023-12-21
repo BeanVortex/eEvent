9376dfaeead5ffe80fb185b65b13e10d8f6780a1
DarkDeveloper_Plant <goldeagle00110011@outlook.com>
2023-12-21 15:47:22 +0330

    -

--- event/urls.py
+++ event/urls.py
@@ -3,7 +3,7 @@ from . import views
 urlpatterns = [
     path("event/index", views.index, name="event_index"),
     path("event/organizer/add/", views.AddEventOrganizer.as_view()),
-    path("event/organizer/edit/<int:id>/", views.EditEventOrganizer.as_view()),
+    path("event/organizer/edit/<int:eid>/", views.EditEventOrganizer.as_view()),
     path("event/organizer/delete/<int:eid>/", views.deleteEventOrganizer),
     path("event/attender/add/<int:eid>/", views.addEventAttender),
     path("event/attender/delete/<int:eid>/", views.deleteEventFromAttender),
